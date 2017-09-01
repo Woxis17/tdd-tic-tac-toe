dfe1195d4dfd1615451efceff11c3b213df00ff0
Woxis17 <robertziaja94@gmail.com>
2017-09-01 14:12:10 +0200

    Change 3rd commit message

--- src/test/java/com/woxis/TicTacToeTest.java
+++ src/test/java/com/woxis/TicTacToeTest.java
@@ -48,4 +48,39 @@ public class TicTacToeTest {
         assertEquals(TicTacToe.O, ticTacToe.nextPlayer());
     }
 
+    @Test
+    public void whenPlayThenNoWinner() {
+        assertEquals(ticTacToe.put(0,0), 0);
+    }
+
+    @Test
+    public void whenPlayerXGot3OnXAxisThenWinner() {
+        assertEquals(ticTacToe.put(0,0), 0);
+        assertEquals(ticTacToe.put(0,1), 0);
+        assertEquals(ticTacToe.put(1,0), 0);
+        assertEquals(ticTacToe.put(1,1), 0);
+        assertEquals(ticTacToe.put(2,0), TicTacToe.X); //WINNER X
+    }
+
+    @Test
+    public void whenPlayerOGot3OnYAxisThenWinner() {
+        assertEquals(ticTacToe.put(0,0), 0);
+        assertEquals(ticTacToe.put(1,0), 0);
+        assertEquals(ticTacToe.put(2,0), 0);
+        assertEquals(ticTacToe.put(1,1), 0);
+        assertEquals(ticTacToe.put(0,1), 0);
+        assertEquals(ticTacToe.put(1,2), TicTacToe.O); //WINNER 0
+    }
+
+    @Test
+    public void givenWinnerWhenNextMoveThenExpectRuntimeException() {
+        assertEquals(ticTacToe.put(0,0), 0);
+        assertEquals(ticTacToe.put(0,1), 0);
+        assertEquals(ticTacToe.put(1,0), 0);
+        assertEquals(ticTacToe.put(1,1), 0);
+        assertEquals(ticTacToe.put(2,0), TicTacToe.X); //WINNER X
+        expectedException.expect(RuntimeException.class);
+        ticTacToe.put(2,2);
+    }
+
 }
